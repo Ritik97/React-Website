535b513de6b5b264d96eea54d83245972a853594
Ritik97 <ritiksinha91@gmail.com>
2021-01-12 18:17:26 +0530

    Added: Navigation Items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar/navbar';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Website</h1>
-    </div>
+    <>
+      <BrowserRouter>
+      <Navbar />
+        <Switch>
+          <Route path='/' exact />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
